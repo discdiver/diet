f3798bc80e3388b175f039dd1bd2d4cc5d54cd2d
Blankie Tails <jeffmshale@gmail.com>
2017-12-03 15:54:26 -0500

    normalize carb column and print sorted column

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A project file to look at carbs and fat and such for Keto diet data science project
+
+Data available free online at USDA  https://www.ars.usda.gov/northeast-area/beltsville-md/beltsville-human-nutrition-research-center/nutrient-data-laboratory/docs/sr28-download-files/
--- script.py
+++ script.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 fd = pd.read_csv("USDA_nutritional_data_2017.csv")
 
-print(fd.head(3))
+# print(fd.head(3))
+
+print(fd.shape)
+
+max_carbs = fd['Carbohydrt_(g)'].max()
+fd['normalized_carbs'] = fd['Carbohydrt_(g)'] / max_carbs
+print(fd['normalized_carbs'])
+
+print(fd.sort_values('normalized_carbs', ascending = False).head(3))
